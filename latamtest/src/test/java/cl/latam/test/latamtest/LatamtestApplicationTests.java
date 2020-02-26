ce64eaab49ca8979524a91ecb6ca8406ff5a1fe6
Fernando <fe.aguileras@gmail.com>
2020-02-26 09:11:41 -0300

    Corrige error

--- latamtest/src/test/java/cl/latam/test/latamtest/LatamtestApplicationTests.java
+++ latamtest/src/test/java/cl/latam/test/latamtest/LatamtestApplicationTests.java
@@ -3,7 +3,6 @@ package cl.latam.test.latamtest;
 import cl.latam.test.latamtest.model.PersonaOutModel;
 import cl.latam.test.latamtest.service.IPersonasService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
